56fb8ef54c11c001c62471362b9ddb2dc1fd27bb
Anthony <antoca29@gmail.com>
2021-03-21 00:46:34 -0600

    Primer commit

---
+++ Patrones/cmake-build-debug/CMakeFiles/Patrones.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Patrones.dir/main.cpp.o"
+  "Patrones"
+  "Patrones.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Patrones.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Patrones/cmake-build-debug/CMakeFiles/Patrones.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Patrones.
+# This may be replaced when dependencies are built.
